7d5786adf393bc4881f06a85b25094d49b77ed06
liziniu <374387855@qq.com>
2019-04-22 14:01:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Mario
+
+### Dependence:
+
+- gym_super_mario_bros
+
+
+
+### How to run 
+
+```
+bash runexp.sh
+```
+
+
+
+
+
+
+
---
+++ runexp.sh
@@ -0,0 +1,7 @@
+python run.py \
+    --env SuperMarioBros-v0 \
+    --env_type atari \
+    --network cnn \
+    --num_env 1 \
+    --reward_scale 0.0667 \
+    --save_video_interval 10
