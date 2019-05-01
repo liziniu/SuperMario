35e4d4da156f939aa7f21f4c1ef6f30d6589742d
liziniu <374387855@qq.com>
2019-05-01 21:41:16 +0800

    fix her_sample her_idx bugs

--- common/her_sample.py
+++ common/her_sample.py
@@ -30,12 +30,12 @@ def make_sample_her_transitions(replay_strategy, replay_k):
         nb_her_sample = len(her_indexes[1])
         offset_indexes = np.random.uniform(size=nb_her_sample) * (T - her_indexes[1])
         future_indexes = np.minimum(np.maximum(1, offset_indexes.astype(int)) + her_indexes[1], T-1)
-        future_indexes = [her_indexes[0], future_indexes]
+        future_indexes = (her_indexes[0], future_indexes)
 
         future_indexes = tuple(future_indexes)
         future_obs = deepcopy(transitions)
         future_obs[her_indexes] = transitions[future_indexes]
-        return future_obs, future_indexes
+        return future_obs, her_indexes, future_indexes
 
     return _sample_her_transitions
 
@@ -44,14 +44,22 @@ if __name__ == "__main__":
     # Test her sample
     import matplotlib.pyplot as plt
     np.random.seed(1)
-    nenv, nsteps, ndim = 1, 10, 1
+    nenv, nsteps, ndim = 2, 10, 1
     sample_fn = make_sample_her_transitions("future", 4)
     goal_obs = np.random.randint(0, 100, [nenv, nsteps, 1], dtype=int)
-    plt.figure()
-    print(goal_obs)
+    for i in range(len(goal_obs.flatten())):
+        print(i, end=",\t")
+    print()
+    for obs in goal_obs.flatten():
+        print(obs, end=",\t")
+    print()
     plt.plot(goal_obs.flatten(), label="origin")
-    goal_obs = sample_fn(goal_obs)
-    print(goal_obs)
-    plt.plot(goal_obs.flatten(), label="her")
-    plt.legend()
-    plt.show()
+    goal_obs, her_index, future_idx = sample_fn(goal_obs)
+    for obs in goal_obs.flatten():
+        print(obs, end=",\t")
+    print("\n--------")
+    for ind in her_index:
+        print(ind)
+    print("--------")
+    for ind in future_idx:
+        print(ind)
