cf63e972e7506b7f66b7b684358eddf67bcc5e86
liziniu <374387855@qq.com>
2019-05-06 13:08:14 +0800

    edify VecFrameStack reset()

--- common/env_util.py
+++ common/env_util.py
@@ -2,7 +2,7 @@ import multiprocessing
 import sys
 import gym
 from collections import defaultdict
-from baselines.common.vec_env import VecFrameStack, VecNormalize
+from baselines.common.vec_env import VecNormalize
 from baselines.common.tf_util import get_session
 import re
 import tensorflow as tf
@@ -15,6 +15,9 @@ from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
 from baselines.common.vec_env.dummy_vec_env import DummyVecEnv
 from baselines.common import retro_wrappers
 import os
+from baselines.common.vec_env import VecEnvWrapper
+import numpy as np
+from gym import spaces
 try:
     from mpi4py import MPI
 except ImportError:
@@ -227,3 +230,30 @@ def parser_env_id(env):
         except:
             pass
     return env_id
+
+
+class VecFrameStack(VecEnvWrapper):
+    def __init__(self, venv, nstack):
+        self.venv = venv
+        self.nstack = nstack
+        wos = venv.observation_space  # wrapped ob space
+        low = np.repeat(wos.low, self.nstack, axis=-1)
+        high = np.repeat(wos.high, self.nstack, axis=-1)
+        self.stackedobs = np.zeros((venv.num_envs,) + low.shape, low.dtype)
+        observation_space = spaces.Box(low=low, high=high, dtype=venv.observation_space.dtype)
+        VecEnvWrapper.__init__(self, venv, observation_space=observation_space)
+
+    def step_wait(self):
+        obs, rews, news, infos = self.venv.step_wait()
+        self.stackedobs = np.roll(self.stackedobs, shift=-1, axis=-1)
+        for (i, new) in enumerate(news):
+            if new:
+                self.stackedobs[..., :-obs.shape[-1]] = obs
+        self.stackedobs[..., -obs.shape[-1]:] = obs
+        return self.stackedobs, rews, news, infos
+
+    def reset(self):
+        obs = self.venv.reset()
+        self.stackedobs[...] = 0
+        self.stackedobs[..., -self.nstack:] = obs
+        return self.stackedobs
