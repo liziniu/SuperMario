cb5dcd990aa2e4b4d99018472206a1045680271c
liziniu <374387855@qq.com>
2019-05-13 16:36:47 +0800

    fix retrace bugs

--- her/model.py
+++ her/model.py
@@ -13,13 +13,14 @@ import numpy as np
 
 # remove last step
 def strip(var, nenvs, nsteps, flat=False):
-    vars = batch_to_seq(var, nenvs, nsteps + 1, flat)
-    return seq_to_batch(vars[:-1], flat)
+    vars = batch_to_seq(var, nenvs, nsteps, flat)
+    return seq_to_batch(vars, flat)
 
 
 def q_retrace(R, D, q_i, v, rho_i, nenvs, nsteps, gamma):
     """
-    Calculates q_retrace targets
+    Calculates q_retrace targets;
+    vs: nenv, nsteps (takes obs_{t+1} and g_t as inputs)
 
     :param R: Rewards
     :param D: Dones
@@ -32,7 +33,7 @@ def q_retrace(R, D, q_i, v, rho_i, nenvs, nsteps, gamma):
     rs = batch_to_seq(R, nenvs, nsteps, True)  # list of len steps, shape [nenvs]
     ds = batch_to_seq(D, nenvs, nsteps, True)  # list of len steps, shape [nenvs]
     q_is = batch_to_seq(q_i, nenvs, nsteps, True)
-    vs = batch_to_seq(v, nenvs, nsteps + 1, True)
+    vs = batch_to_seq(v, nenvs, nsteps, True)   # (by lizn, only the next state value)
     v_final = vs[-1]
     qret = v_final
     qrets = []
@@ -40,7 +41,8 @@ def q_retrace(R, D, q_i, v, rho_i, nenvs, nsteps, gamma):
         check_shape([qret, ds[i], rs[i], rho_bar[i], q_is[i], vs[i]], [[nenvs]] * 6)
         qret = rs[i] + gamma * qret * (1.0 - ds[i])
         qrets.append(qret)
-        qret = (rho_bar[i] * (qret - q_is[i])) + vs[i]
+        if i > 0:
+            qret = (rho_bar[i] * (qret - q_is[i])) + vs[i-1]
     qrets = qrets[::-1]
     qret = seq_to_batch(qrets, flat=True)
     return qret
@@ -54,8 +56,9 @@ def q_retrace(R, D, q_i, v, rho_i, nenvs, nsteps, gamma):
 
 class Model(object):
     def __init__(self, sess, policy, ob_space, ac_space, nenvs, nsteps, ent_coef, q_coef, gamma,
-                 max_grad_norm, lr, rprop_alpha, rprop_epsilon, total_timesteps, lrschedule, c,
-                 trust_region, alpha, delta, scope, goal_shape):
+                 max_grad_norm, lr,
+                 rprop_alpha, rprop_epsilon, total_timesteps, lrschedule, c, trust_region, alpha, delta, scope,
+                 goal_shape):
         self.sess = sess
         self.nenv = nenvs
         self.goal_shape = goal_shape
@@ -79,6 +82,8 @@ class Model(object):
             self.MU = tf.placeholder(tf.float32, [nbatch, nact], name="mus")  # mu's
             self.LR = tf.placeholder(tf.float32, [], name="lr")
 
+            self.V_NEXT = tf.placeholder(tf.float32, [nbatch], name="value_next")  # (by lzn: we revise goal-conditioned next value)
+
             step_ob_placeholder = tf.placeholder(ob_space.dtype, (nenvs,) + ob_space.shape, "step_ob")
             if goal_as_image:
                 step_goal_placeholder = tf.placeholder(ob_space.dtype, (nenvs,) + ob_space.shape, "step_goal")
@@ -89,13 +94,13 @@ class Model(object):
                     (step_goal_placeholder - self.goal_rms.mean) / self.goal_rms.std,
                     -5., 5.)
 
-            train_ob_placeholder = tf.placeholder(ob_space.dtype, (nenvs * (nsteps + 1),) + ob_space.shape, "train_ob")
+            train_ob_placeholder = tf.placeholder(ob_space.dtype, (nenvs * nsteps,) + ob_space.shape, "train_ob")
             if goal_as_image:
-                train_goal_placeholder = tf.placeholder(ob_space.dtype, (nenvs * (nsteps + 1),) + ob_space.shape,
+                train_goal_placeholder = tf.placeholder(ob_space.dtype, (nenvs * nsteps,) + ob_space.shape,
                                                         "train_goal")
                 concat_on_latent, train_goal_encoded = False, None
             else:
-                train_goal_placeholder = tf.placeholder(tf.float32, (nenvs * (nsteps + 1),) + goal_shape,
+                train_goal_placeholder = tf.placeholder(tf.float32, (nenvs * nsteps,) + goal_shape,
                                                         "train_goal")
                 concat_on_latent = True
                 train_goal_encoded = tf.clip_by_value(
@@ -141,10 +146,13 @@ class Model(object):
         train_model_p = tf.nn.softmax(self.train_model.pi)
         polyak_model_p = tf.nn.softmax(self.polyak_model.pi)
         self.step_model_p = tf.nn.softmax(self.step_model.pi)
-        v = tf.reduce_sum(train_model_p * self.train_model.q, axis=-1)  # shape is [nenvs * (nsteps + 1)]
+        # (todo by lizn, use this to calculate next value)
+        v = self.v = tf.reduce_sum(train_model_p * self.train_model.q, axis=-1)  # shape is [nenvs * (nsteps)]
 
         # strip off last step
+        # (todo by lizn, we don't need strip)
         f, f_pol, q = map(lambda var: strip(var, nenvs, nsteps), [train_model_p, polyak_model_p, self.train_model.q])
+        # f, f_pol, q = map(lambda x: x, [train_model_p, polyak_model_p, self.train_model.q])
         # Get pi and q values for actions taken
         f_i = get_by_index(f, self.A)
         q_i = get_by_index(q, self.A)
@@ -154,7 +162,7 @@ class Model(object):
         rho_i = get_by_index(rho, self.A)
 
         # Calculate Q_retrace targets
-        qret = q_retrace(self.R, self.D, q_i, v, rho_i, nenvs, nsteps, gamma)
+        qret = q_retrace(self.R, self.D, q_i, self.V_NEXT, rho_i, nenvs, nsteps, gamma)  # (todo by lizn, use new next state value)
 
         # Calculate losses
         # Entropy
@@ -162,7 +170,7 @@ class Model(object):
         entropy = tf.reduce_mean(cat_entropy_softmax(f))
 
         # Policy Graident loss, with truncated importance sampling & bias correction
-        v = strip(v, nenvs, nsteps, True)
+        v = strip(v, nenvs, nsteps, True)  # (todo by lzn: we do not need the strip the last one)
         check_shape([qret, v, rho_i, f_i], [[nenvs * nsteps]] * 4)
         check_shape([rho, f, q], [[nenvs * nsteps, nact]] * 2)
 
@@ -226,6 +234,7 @@ class Model(object):
         grads = list(zip(grads, params))
         trainer = tf.train.RMSPropOptimizer(learning_rate=self.LR, decay=rprop_alpha, epsilon=rprop_epsilon)
         _policy_opt_op = trainer.apply_gradients(grads)
+
         # so when you call _train, you first do the gradient step, then you apply ema
         with tf.control_dependencies([_policy_opt_op]):
             _train_policy = tf.group(ema_apply_op)
@@ -250,10 +259,17 @@ class Model(object):
         self.initial_state = self.step_model.initial_state
         tf.global_variables_initializer().run(session=self.sess)
 
-    def train_policy(self, obs, actions, rewards, dones, mus, states, masks, steps, goal_obs, verbose=False):
+    def train_policy(self, obs, next_obs, actions, rewards, dones, mus, states, masks, steps, goal_obs, verbose=False):
         cur_lr = self.lr.value_steps(steps)
+        # 1. calculate v_{t+1} using obs_{t+1} and g_t
+        td_map = {self.train_model.X: next_obs}
+        assert hasattr(self.train_model, "goals")
+        td_map[self.train_model.goals] = goal_obs
+        v_next = self.sess.run(self.v, feed_dict=td_map)
+        # 2. use obs_t, goal_t, v_{t+1} to train policy
         td_map = {self.train_model.X: obs, self.polyak_model.X: obs, self.A: actions, self.R: rewards, self.D: dones,
-                  self.MU: mus, self.LR: cur_lr}
+                  self.MU: mus, self.LR: cur_lr, self.V_NEXT: v_next}
+
         assert hasattr(self.train_model, "goals")
         assert hasattr(self.polyak_model, "goals")
         if hasattr(self, "goal_rms"):
